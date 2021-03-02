da5c66141dac5e01aa52e8be8bb96996f447d1b0
Angelo Cutaia <angelo.cutaia@linksfoundation.com>
2021-03-02 14:30:38 +0100

    Add track models
    Signed-off-by: Angelo <angelo.cutaia@linksfoundation.com>

---
+++ app/models/track.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+Track Models
+
+:author: Angelo Cutaia
+:copyright: Copyright 2020, Angelo Cutaia
+:version: 1.0.0
+
+..
+
+    Copyright 2020 Angelo Cutaia
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Standard Library
+from enum import Enum
+from typing import TypeVar, List, Optional, Any
+
+# --------------------------------------------------------------------------------------------
+
+TrackSegments = TypeVar("TrackSegments", bound=List[Optional[Any]])
+"""Track Segments list"""
+
+
+class TypeOfTrack(str, Enum):
+    """Type of track"""
+    private = "private"
+    commuting = "commuting"
+    business_trip = "business trip"
+
+
+class RequestType(str, Enum):
+    """Track requested"""
+    partial_mobility = "Partial_Mobility"
+    complete_mobility = "Complete_Mobility"
+    all_positions = "All_Positions"
+    stats_num_tracks = "Stats_num_tracks"
+    stats_avg_time = "Stats_avg_time"
+    stats_avg_space = "Stats_avg_space"
+
+
+class TypeDay(str, Enum):
+    """Type of day"""
+    working_day = "Working_Day"
+    week_end = "Week_End"
+    any = "Any"
+
+
+class MobilityType(str, Enum):
+    """Type of mobility"""
+    any = "any"
+    walk = "walk"
+    bike = "bike"
+    train = "train"
+    car = "car"
+    bus = "bus"
+    airplane = "airplane"
+    boat = "boat"
+
+
+class DetectionType(str, Enum):
+    """How the track was detected"""
+    user = "user"
+    app = "app"
+    third_party = "third_party"
+    any = "any"
+
+
+class AggregationType(str, Enum):
+    """Aggregation of the info"""
+    space = "space"
+    time = "time"
+
+# --------------------------------------------------------------------------------------------
+
+
+
