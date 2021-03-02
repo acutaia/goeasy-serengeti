17e54aee635583aadce242833942fe2da8d91b41
Angelo Cutaia <angelo.cutaia@linksfoundation.com>
2021-03-02 14:52:49 +0100

    Add cython module to calculate if the lat e lon of a point are near Italy or Sweden
    Signed-off-by: Angelo <angelo.cutaia@linksfoundation.com>

---
+++ app/internals/position_alteration_detection.pyx
@@ -0,0 +1,82 @@
+#!python
+#cython: language_level=3
+
+"""
+Haversine function
+
+:author: Angelo Cutaia
+:copyright: Copyright 2020, Angelo Cutaia
+:version: 1.0.0
+
+..
+
+    Copyright 2020 Angelo Cutaia
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+# Standard c++ library
+from libc.math cimport sin, cos, asin, sqrt
+
+# -------------------------------------------------------------------------------------------
+
+# CONSTANTS
+cdef double ITALY_LAT, ITALY_LON, PHI_ITALY, THETA_ITALY
+cdef double SWEDEN_LAT, SWEDEN_LON, PHI_SWEDEN, THETA_SWEDEN
+cdef str ITALY, SWEDEN
+
+# ITALY
+ITALY_LAT = 45.0781
+ITALY_LON = 7.6761
+PHI_ITALY = ITALY_LAT * 0.0174532925
+THETA_ITALY = ITALY_LON * 0.0174532925
+ITALY = "Italy"
+
+# SWEDEN
+SWEDEN_LAT = 59.3261
+SWEDEN_LON = 18.0232
+PHI_SWEDEN = SWEDEN_LAT * 0.0174532925
+THETA_SWEDEN = SWEDEN_LON * 0.0174532925
+SWEDEN = "Sweden"
+
+# -------------------------------------------------------------------------------------------
+
+
+def haversine(double lat, double lon) -> str:
+    """Given two (lat, lng) tuples, returns the distance between them in
+    meters."""
+    cdef double lat1, lng1, ph1, theta1
+    cdef double dphi_italy, dtheta_italy, a_italy, italy_distance
+    cdef double dphi_sweden, dtheta_sweden , a_sweden, sweden_distance
+
+    lat1 = lat
+    lng1 = lon
+
+    ph1 = lat1 * 0.0174532925
+    theta1 = lng1 * 0.0174532925
+
+    dphi_italy = PHI_ITALY - ph1
+    dphi_sweden = PHI_SWEDEN -ph1
+
+    dtheta_italy = THETA_ITALY - theta1
+    dtheta_sweden = THETA_SWEDEN - theta1
+
+    a_italy = sin(dphi_italy/2)**2 +cos(ph1) * cos(PHI_ITALY) * sin(dtheta_italy/2)**2
+    a_sweden = sin(dphi_sweden/2)**2 +cos(ph1) * cos(PHI_SWEDEN) * sin(dtheta_sweden/2)**2
+
+    italy_distance = 2 * asin(sqrt(a_italy)) * 6367444.7
+    sweden_distance = 2 * asin(sqrt(a_sweden)) * 6367444.7
+
+    if italy_distance < sweden_distance:
+        return ITALY
+
+    return SWEDEN
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Building app.utilities.haversine module
+
+:author: Angelo Cutaia
+:copyright: Copyright 2020, Angelo Cutaia
+:version: 1.0.0
+
+..
+
+    Copyright 2020 Angelo Cutaia
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension(
+        "app.internals.position_alteration_detection",    # location of the resulting .so
+        ["app/internals/position_alteration_detection.pyx"],
+    )
+]
+
+
+setup(
+    name='package',
+    packages=find_packages(),
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+)
