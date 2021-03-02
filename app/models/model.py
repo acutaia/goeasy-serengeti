fa5609d1b2df003abeb6bdd44e000e0e45311de8
Angelo Cutaia <angelo.cutaia@linksfoundation.com>
2021-03-02 14:31:27 +0100

    Add Orjson Model
    Signed-off-by: Angelo <angelo.cutaia@linksfoundation.com>

---
+++ app/models/model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Base models package
+
+:author: Angelo Cutaia
+:copyright: Copyright 2020, Angelo Cutaia
+:version: 1.0.0
+
+..
+
+    Copyright 2020 Angelo Cutaia
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+import orjson
+from pydantic import BaseModel
+
+# --------------------------------------------------------------------------------------------
+
+
+def orjson_dumps(v, *, default):
+    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+    return orjson.dumps(v, default=default).decode()
+
+
+class OrjsonModel(BaseModel):
+    """Model that use orjson to serialize and deserialize"""
+
+    class Config:
+        # Use orjson to improve performance
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+
