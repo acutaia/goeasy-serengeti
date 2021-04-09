d02f8e8ef24f7480aeefb0ae867bf3f1afa2aca0
acutaia <angeloxx92@hotmail.it>
2021-04-09 11:12:49 +0200

    Removed semaphore from keycloack

--- app/internals/keycloak.py
+++ app/internals/keycloak.py
@@ -24,19 +24,18 @@ Keycloack package
 """
 
 # Standard Library
-from asyncio import Lock, Semaphore
+from asyncio import Lock
 from functools import lru_cache
 import time
 
 # Third Party
 from fastapi import status, HTTPException
-from aiohttp import ClientSession, ClientResponseError, ClientTimeout, ServerTimeoutError
+from aiohttp import ClientSession, ClientResponseError, ClientTimeout, ServerTimeoutError, TCPConnector
 import orjson
 
 # Internal
 from .logger import get_logger
 from ..config import get_keycloack_settings
-from ..models.model import orjson_dumps
 from ..models.security import Token
 
 # --------------------------------------------------------------------------------------------
@@ -54,20 +53,37 @@ class _Keycloak:
     async_lock: Lock
     """Asynchronous lock """
 
-    async_semaphore: Semaphore
-    """Asynchronous semaphore"""
+    session: ClientSession
+    """Aiohttp session"""
 
     async def setup(self):
         """
         Setup keycloack, call this method only inside the startup event
         """
+
+        # timeout to obtain  token
+        timeout = ClientTimeout(total=10)
+        # connection options
+        connector = TCPConnector(ttl_dns_cache=300, limit=1, verify_ssl=False)
+
+        # Instantiate a session
+        self.session = ClientSession(
+                    raise_for_status=True,
+                    json_serialize=lambda x: orjson.dumps(x).decode(),
+                    timeout=timeout,
+                    connector=connector
+            )
         self.async_lock = Lock()
-        self.async_semaphore = Semaphore(30)
         self.last_token = await self._get_token()
         self.last_token_reception_time = time.time()
 
-    @classmethod
-    async def _get_token(cls):
+    async def close(self):
+        """
+        Close gracefully Keycloack session
+        """
+        await self.session.close()
+
+    async def _get_token(self):
         """
         Private method used to make the http request to keycloack
         in order to obtain a valid token
@@ -77,27 +93,24 @@ class _Keycloak:
         # Get settings
         settings = get_keycloack_settings()
 
-        # timeout to obtain  token
-        timeout = ClientTimeout(total=30)
         try:
-            async with ClientSession(raise_for_status=True, json_serialize=orjson_dumps, timeout=timeout) as session:
-                async with session.post(
-                    url=settings.token_request_url,
-                    data={
-                        "client_id": settings.client_id,
-                        "username": settings.username_keycloak,
-                        "password": settings.password,
-                        "grant_type": settings.grant_type,
-                        "client_secret": settings.client_secret
-                    }
-                ) as resp:
-                    return Token.parse_obj(
-                        await resp.json(
-                            encoding="utf-8",
-                            loads=orjson.loads,
-                            content_type=None
-                        )
-                    ).access_token
+            async with self.session.post(
+                url=settings.token_request_url,
+                data={
+                    "client_id": settings.client_id,
+                    "username": settings.username_keycloak,
+                    "password": settings.password,
+                    "grant_type": settings.grant_type,
+                    "client_secret": settings.client_secret
+                }
+            ) as resp:
+                return Token.parse_obj(
+                    await resp.json(
+                        encoding="utf-8",
+                        loads=orjson.loads,
+                        content_type=None
+                    )
+                ).access_token
 
         except ClientResponseError as exc:
             await logger.error(
@@ -136,40 +149,38 @@ class _Keycloak:
         it will obtain a fresh one and adjust it's timestamp
         """
 
-        # Use an a semaphore to let pass 100 coroutines
-        async with self.async_semaphore:
-            # timestamp of this moment
-            now = time.time()
+        # Obtain actual timestamp
+        now = time.time()
 
-            # Check if it's been at least 50 seconds since last token was obtained
-            if now - self.last_token_reception_time > 50:
+        # Check if it's been at least 50 seconds since last token was obtained
+        if now - self.last_token_reception_time >= 150:
 
-                # Check if the lock is already acquired by a coroutine
-                if self.async_lock.locked():
+            # Check if the lock is already acquired by a coroutine
+            if self.async_lock.locked():
 
-                    # Await until the lock is released by the other coroutine
-                    # and after that release it
-                    await self.async_lock.acquire()
-                    self.async_lock.release()
+                # Await until the lock is released by the other coroutine
+                # and after that release it
+                await self.async_lock.acquire()
+                self.async_lock.release()
 
-                    # Here we are sure that the token was updated
-                    return self.last_token
+                # Here we are sure that the token was updated
+                return self.last_token
 
-                # Only one coroutine has to update the token
-                async with self.async_lock:
-                    # Update token and timestamp
-                    self.last_token = await self._get_token()
-                    self.last_token_reception_time = time.time()
+            # Only one coroutine has to update the token
+            async with self.async_lock:
+                # Update token and timestamp
+                self.last_token = await self._get_token()
+                self.last_token_reception_time = time.time()
 
-            # return the stored token
-            return self.last_token
+        # return the stored token
+        return self.last_token
 
 # --------------------------------------------------------------------------------------------
 
 
 @lru_cache(maxsize=1)
 def _get_keycloack() -> _Keycloak:
-    """Instantiate a singleton  Keycloack"""
+    """Instantiate a singleton Keycloack"""
     return _Keycloak()
 
 # --------------------------------------------------------------------------------------------
